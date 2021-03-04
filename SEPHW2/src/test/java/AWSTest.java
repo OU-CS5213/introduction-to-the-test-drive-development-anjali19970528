1e326db18e856bcce8afbd951cd3338062ab62e6
anjali19970528 <78509311+anjali19970528@users.noreply.github.com>
2021-03-04 00:08:29 -0600

    Update AWSTest.java

--- SEPHW2/src/test/java/AWSTest.java
+++ SEPHW2/src/test/java/AWSTest.java
@@ -121,7 +121,7 @@ class AWSTest {
 		int[] org = originalAWS.getValues();
 		int expectedValue = org[position];
  		int first = org[0];
-		int expected = originalAWS.getValues().length + MATH.abs(numberOfTimes);
+		int expected = originalAWS.getValues().length + Math.abs(numberOfTimes);
 		originalAWS.fillAndExpand(position, numberOfTimes);
 		int[] result = originalAWS.getValues();
 		assertEquals(expected, result.length);
